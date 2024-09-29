82c75da4d95084a941af4dd1c5f857b8a46e14bd
abdifatahmuse <cabdifataaxmuuse@gmail.com>
2024-09-29 19:51:08 +0300

    the object orient

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -3,27 +3,45 @@
 
 
 class Book:
-    # TODO: Properties defined at the class level are shared by all instances
-
+    # TODO: Properties defined at the class level are shared by  all instances
+    BookType = ("Hardcover", "Paperback", "Ebook")
+    
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
     # TODO: create a class method
+    @classmethod
+    def get_book_types(cls):
+        return cls.BookType
 
     # TODO: create a static method
+    def getbooklist():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BookType):
+            raise ValueError(f"{booktype} is not a valid Book Type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
-
+print("Book types: ", Book.get_book_types())
 
 # TODO: Create some book instances
-
+b1 = Book("Title 1", "Hardcover")
+b2 = Book("Title 2", "Paperback")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print(thebooks)
